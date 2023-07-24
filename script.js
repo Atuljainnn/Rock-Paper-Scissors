c360efec6b2852556f24e02617ef53588e87957c
atuljainnn <mratul3877@gmail.com>
2023-07-24 23:05:28 +0530

    Use advanced functions

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
 
-let score =JSON.parse(localStorage.getItem('score'));
-    
-if(!score) {
-  score={
-  wins:0,
+let score =JSON.parse(localStorage.getItem('score'))||{ wins:0,
   losses:0,
   ties:0
-}
-}
+};
+    
 
 updateScore();
 
@@ -16,7 +12,7 @@ let intervalId;
 
 function autoPlay(){
   if(!isAutoPlaying){
-    intervalId=setInterval(function(){
+    intervalId=setInterval(()=>{
       const playerMove=pickComputerMove();
       playGame(playerMove);
      },1000);
@@ -28,8 +24,45 @@ function autoPlay(){
     isAutoPlaying=false;
   }
  
-
 }
+
+document.querySelector('.js-rock-button').addEventListener('click',()=>{
+    playGame('rock')
+});
+
+document.querySelector('.js-paper-button').addEventListener('click',()=>{
+  playGame('paper')
+});
+
+
+document.querySelector('.js-scissors-button').addEventListener('click',()=>{
+  playGame('scissors')
+});
+
+document.querySelector('.js-reset-button').addEventListener('click',()=>{
+  score.wins=0;
+  score.losses=0;
+  score.ties=0;
+  localStorage.removeItem('score');
+  updateScore();
+});
+
+document.querySelector('.js-auto-play').addEventListener('click',()=>{
+  autoPlay();
+});
+
+document.body.addEventListener('keydown',(event)=>{
+  if(event.key==='r'){
+    playGame('rock');
+  }
+  else if(event.key==='p'){
+    playGame('paper')
+  }
+  else if(event.key==='s'){
+    playGame('scissors');
+  }
+})
+
 
 function playGame( playerMove){
 
